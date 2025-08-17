a3c94cbb2f7db3588da884923b0a2435c877655f
Shaness-BotDev <iamshanesareu@gmail.com>
2025-08-18 01:11:55 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -2,10 +2,10 @@ import os
 
 def load_settings():
     settings = {
-        "DISCORD_TOKEN": os.getenv("DISCORD_BOT_TOKEN")
+        "DISCORD_BOT_TOKEN": os.getenv("DISCORD_BOT_TOKEN")
     }
 
-    if not settings["DISCORD_TOKEN"]:
+    if not settings["DISCORD_BOT_TOKEN"]:
         raise ValueError("❌ DISCORD_TOKEN is not set in environment variables.")
 
     return settings
